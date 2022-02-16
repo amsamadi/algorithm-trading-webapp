e388aaf96fa3acaf8f436bdc4c0354add7a058cb
Amjad Samadi <87423904+amsamadi@users.noreply.github.com>
2022-02-17 01:02:27 +0330

    create script for deploy project

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+which docker-compose &> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: docker-compose is not installed."
+    exit 1
+fi
+
+docker network create mynet
+
+docker-compose up -d
+
+cat <<EOL 
+
+you can access the web application with this URL: http://{YOUR-SYSTEM-IP}:5352
+
+EOL
